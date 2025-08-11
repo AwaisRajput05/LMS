2b60e81a0df7a2e6bb6a6095295689578dfc17d6
AwaisRajput05 <mawais786rana@gmail.com>
2025-08-11 10:45:09 -0700

    change in try block

--- backend/controllers/webhooks.js
+++ backend/controllers/webhooks.js
@@ -7,13 +7,13 @@ export const clerkWebhooks = async (req, res)=>{
 
     try{
         const webhook= new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        await webhook.verify(json.stringify(req.body),{
+        await webhook.verify(JSON .stringify(req.body),{
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
         })
         const{ data,type} = req.body;
-        switch(key){
+        switch(type){
             case "user.created" :{
                 const userData = {
                     _id: data.id,
@@ -29,7 +29,7 @@ export const clerkWebhooks = async (req, res)=>{
             case "user.updated" :{
                 const userData = {
                     
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url
                 }
